8b8a5b0bf14001db148b4cc0d517bf126b054cc8
ryotai-trpg <ryotai1025@yahoo.co.jp>
2024-12-27 22:30:13 +0900

    update itemiconchange macro

--- macro/itemiconchange.js
+++ macro/itemiconchange.js
@@ -1,58 +1,73 @@
-// Compendiumアイテムアイコン一括変更
+// 辞典アイテムアイコン一括変更
+// compendiumKeysを変更したい辞典のIDに変更し、マクロ実行してください。ActorとItemの辞典に対応しています。
+// 旧デフォルトアイコン（アイテム袋）のものをシステムデフォルトのアイコンに変更します。
 
-// デフォルト設定
-/*
-武器 weapon icons/svg/sword.svg
-防具 armor icons/svg/shield.svg
-装飾品 accessory icons/svg/wing.svg
-アイテム item icons/svg/item-bag.svg
-魔法 spell icons/svg/daze.svg
-その他特技 enhancearts magicalsong ridingtrick alchemytech phasearea tactics icons/svg/upgrade.svg
-判定 check icons/svg/circle.svg
-リソース resource icons/svg/coin.svg
-戦闘特技 combatability icons/svg/combat.svg
-技能 skill icons/svg/regen.svg
-種族特徴 raceability icons/svg/paralysis.svg
-言語 language icons/svg/book.svg
-魔物能力 monsterability icons/svg/skull.svg
-*/
+//ここを変更
+const compendiumKeys = [
+ "sw25.actor", 
+ "sw25.item"
+];
 
-// Compendium指定
-const compendiumKey = "sw25compendium.item";
-// 条件
-const searchKey = "type";
-const searchValue = "weapon";
-// 変更データ
-const iconPath = "icons/svg/sword.svg";
+let type_icon_pairs = {
+  'weapon':'icons/svg/sword.svg', //武器
+  'armor':'icons/svg/shield.svg', //防具
+  'accessory':'icons/svg/wing.svg', //装飾品
+  'item':'icons/svg/item-bag.svg', //アイテム
+  'spell':'icons/svg/daze.svg', //魔法
+  'enhancearts': 'icons/svg/upgrade.svg', //練技
+  'magicalsong': 'icons/svg/upgrade.svg', //呪歌と終律
+  'ridingtrick': 'icons/svg/upgrade.svg', //騎芸
+  'alchemytech': 'icons/svg/upgrade.svg', //賦術
+  'phasearea': 'icons/svg/upgrade.svg', //相域
+  'tactics': 'icons/svg/upgrade.svg', //鼓砲・陣率
+  'check':'icons/svg/circle.svg', //判定
+  'resource':'icons/svg/coins.svg', //リソース
+  'combatability':'icons/svg/combat.svg', //戦闘特技
+  'skill':'icons/svg/regen.svg', //技能
+  'raceability':'icons/svg/paralysis.svg', //種族特徴
+  'language':'icons/svg/book.svg', //言語
+  'monsterability':'icons/svg/skull.svg' //魔物能力
+};  
+
+// メイン処理の関数
+function changeItemsIcon(items) {
+  for (let [searchValue, iconPath] of Object.entries(type_icon_pairs)) {
+    // フィルタリング
+    let filteredItems = items.filter((item) => item["type"] === searchValue);
+
+    // 更新
+    for (let item of filteredItems) {
+      if (item.img == "icons/svg/item-bag.svg"){
+        let updatedData = {
+          img: iconPath,
+        };
+        item.update(updatedData);
+      }
+    }
+  }
+}
 
 (async () => {
-  // コンペンディウム取得
-  let pack = game.packs.get(compendiumKey);
-  if (!pack) {
-    ui.notifications.error(
-      "指定されたコンペンディウムパックが見つかりません。"
-    );
-    return;
+  // 辞典取得
+  for (let compendiumKey of compendiumKeys) {
+    let pack = game.packs.get(compendiumKey);
+    if (!pack) {
+      ui.notifications.error( `"${compendiumKey}" が見つかりません。`);
+    } else {
+      if (pack.metadata.type == "Item"){
+        let items = await pack.getDocuments();
+        await changeItemsIcon(items);
+      } else if (pack.metadata.type == "Actor"){
+        let actors = await pack.getDocuments();
+        for (let actor of actors) {
+          let items = actor.items;
+          await changeItemsIcon(items);
+        }
+      } else {
+        ui.notifications.error( `"${compendiumKey}" はキャラクターとアイテム以外の辞典です。`);
+      }
+    }
   }
-
-  // コンペンディウム全アイテム取得
-  let items = await pack.getDocuments();
-
-  // フィルタリング
-  let filteredItems = items.filter((item) => item[searchKey] === searchValue);
-
-  if (filteredItems.length === 0) {
-    ui.notifications.warn("条件に合致するアイテムが見つかりません。");
-    return;
-  }
-
-  // 更新
-  for (let item of filteredItems) {
-    let updatedData = {
-      img: iconPath,
-    };
-    await item.update(updatedData);
-  }
-
   ui.notifications.info("条件に合致するアイテムが更新されました。");
 })();
+
