0a6aac728c76d08aa81d0f456c0e67a37a3e2e14
jeannjeann <136861083+jeannjeann@users.noreplay.github.com>
2025-06-09 11:26:02 +0900

    rollreqbtn for v13

--- module/sw25.mjs
+++ module/sw25.mjs
@@ -989,7 +989,7 @@ Hooks.once("ready", async function () {
 
 // SceneControl Hook
 Hooks.on("getSceneControlButtons", function (controls) {
-  controls[0].tools.push({
+  const rollRequestBtn = {
     name: "rollRequest(Skill)",
     title: game.i18n.localize("SETTING.rollRequest"),
     icon: "fas fa-dice-d6",
@@ -998,7 +998,24 @@ Hooks.on("getSceneControlButtons", function (controls) {
       rollreq();
     },
     button: true,
-  });
+  };
+  const isV13Plus = foundry.utils.isNewerVersion(game.version, "13");
+  // v13 or newer
+  if (isV13Plus) {
+    if (!controls.tokens.tools) controls.tokens.tools = {};
+    controls.tokens.tools[rollRequestBtn.name] = rollRequestBtn;
+  }
+  // v12 or older
+  else {
+    if (controls[0] && controls[0].tools) {
+      const existingButton = controls[0].tools.find(
+        (tool) => tool.name === rollRequestBtn.name
+      );
+      if (!existingButton) {
+        controls[0].tools.push(rollRequestBtn);
+      }
+    }
+  }
 });
 
 // textarea edit hook
